5b86e5a1392f8e7f991e8609f2277d7f745461a5
Artashes Balabekyan <balabekyanartashes@gmail.com>
2023-06-29 05:47:45 +0000

    make iterator public
    Signed-off-by: Artashes Balabekyan <balabekyanartashes@gmail.com>

--- client/iterator.go
+++ client/iterator.go
@@ -2,7 +2,7 @@ package client
 
 import "context"
 
-type iterator struct {
+type Iterator struct {
 	ch     <-chan string
 	ctx    context.Context
 	cancel context.CancelCauseFunc
@@ -14,7 +14,7 @@ type iterator struct {
 // It returns true if and only if there is a new key
 // available. If there are no more keys or an error
 // has been encountered, Next returns false.
-func (i *iterator) Next() (string, bool) {
+func (i *Iterator) Next() (string, bool) {
 	select {
 	case v, ok := <-i.ch:
 		return v, ok
@@ -25,7 +25,40 @@ func (i *iterator) Next() (string, bool) {
 
 // Err returns the first error, if any, encountered
 // while iterating over the set of keys.
-func (i *iterator) Close() error {
+func (i *Iterator) Close() error {
+	// i.cancel(context.Canceled)
+	return context.Cause(i.ctx)
+}
+
+type namePayload struct {
+	name    string
+	payload []byte
+}
+
+type IteratorWithPayload struct {
+	ch     <-chan namePayload
+	ctx    context.Context
+	cancel context.CancelCauseFunc
+}
+
+// Next moves the iterator to the next key, if any.
+// This key is available until Next is called again.
+//
+// It returns true if and only if there is a new key
+// available. If there are no more keys or an error
+// has been encountered, Next returns false.
+func (i *IteratorWithPayload) Next() (namePayload, bool) {
+	select {
+	case v, ok := <-i.ch:
+		return v, ok
+	case <-i.ctx.Done():
+		return namePayload{}, false
+	}
+}
+
+// Err returns the first error, if any, encountered
+// while iterating over the set of keys.
+func (i *IteratorWithPayload) Close() error {
 	// i.cancel(context.Canceled)
 	return context.Cause(i.ctx)
 }
--- client/secrets.go
+++ client/secrets.go
@@ -107,7 +107,7 @@ func (s *Connection) DeleteSecret(ctx context.Context, name string) error {
 // concurrent changes to the Barbican - i.e.
 // creates or deletes. Further, it does not provide any
 // ordering guarantees.
-func (s *Connection) ListSecrets(ctx context.Context) (*iterator, error) {
+func (s *Connection) ListSecrets(ctx context.Context) (*Iterator, error) {
 	var cancel context.CancelCauseFunc
 	ctx, cancel = context.WithCancelCause(ctx)
 	values := make(chan string, 10)
@@ -149,7 +149,7 @@ func (s *Connection) ListSecrets(ctx context.Context) (*iterator, error) {
 			}
 		}
 	}()
-	return &iterator{
+	return &Iterator{
 		ch:     values,
 		ctx:    ctx,
 		cancel: cancel,
